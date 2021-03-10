73122ebac820b59f7f3f459b773f9bd0e5ec0ae7
peterhuang <peterhuang@inquartik.com>
2021-03-11 02:47:19 +0800

    layout

--- js/eBookList.js
+++ js/eBookList.js
@@ -1,26 +1,30 @@
 const menuList = [
   {
-    name: "Item1",
+    name: "基礎服務",
     subList: [
       {
-        name: "subItem1_1",
+        name: "服務人員專業度",
         url: "subItem1_1.html"
       },
       {
-        name: "subItem1_2",
+        name: "服務設施合宜程度",
         url: "subItem1_2.html"
       },
       {
-        name: "subItem1_3",
+        name: "網站使用便利性",
         url: "subItem1_3.html"
+      },
+      {
+        name: "服務資訊透明度",
+        url: "subItem1_4.html"
       },
     ]
   },
   {
-    name: "Item2",
+    name: "服務遞送",
     subList: [
       {
-        name: "subItem2_1",
+        name: "建置單一窗口整合服務",
         subList: [
           {
             name: "subItem2_1_1",
@@ -33,11 +37,11 @@ const menuList = [
         ]
       },
       {
-        name: "subItem2_2",
+        name: "減除申辦案件需檢附之書表謄本",
         url: "subItem2_2.html"
       },
       {
-        name: "subItem2_3",
+        name: "開發線上申辦或跨平台通用服務",
         subList: [
           {
             name: "subItem2_3_1",
@@ -53,20 +57,66 @@ const menuList = [
           },
         ]
       },
+      {
+        name: "突破成長",
+        url: "subItem2_2.html"
+      },
+      {
+        name: "歷年服務再精進",
+        url: "subItem2_2.html"
+      },
     ]
   },
   {
-    name: "Item3",
+    name: "服務能量",
     subList: [
       {
-        name: "subItem3_1",
+        name: "內部簡化作業",
         url: "subItem3_1.html"
       },
       {
-        name: "subItem3_2",
+        name: "服務精進機制",
         url: "subItem3_2.html"
       },
     ]
+  },
+  {
+    name: "服務評價",
+    subList: [
+      {
+        name: "服務滿意情形",
+        url: "subItem3_1.html"
+      },
+    ]
+  },
+  {
+    name: "開放創新",
+    subList: [
+      {
+        name: "開放參與",
+        url: "subItem3_1.html"
+      },
+      {
+        name: "創新性",
+        url: "subItem3_2.html"
+      },
+    ]
+  },
+  {
+    name: "第一線為民服務情形",
+    url: "subItem3_2.html"
+  },
+  {
+    name: "地政業務行銷宣導有效程度",
+    url: "subItem3_2.html"
+  },
+  {
+    name: "電話禮貌測試",
+    url: "subItem3_2.html"
+  },
+  {
+    name: "綜合業務",
+    url: "subItem3_2.html"
   },
 
 ]
